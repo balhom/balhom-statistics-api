aaecfc3dccf35e58abca10e3b25fc125a8014075
fabbo-repo <fscherle98@gmail.com>
2025-06-16 11:13:33 +0200

    Kafka connection improvements

--- src/main/kotlin/org/balhom/statisticsapi/modules/currencyprofilechanges/infrastructure/consumers/data/CurrencyProfileChangeEvent.kt
+++ src/main/kotlin/org/balhom/statisticsapi/modules/currencyprofilechanges/infrastructure/consumers/data/CurrencyProfileChangeEvent.kt
@@ -1,11 +1,13 @@
 package org.balhom.statisticsapi.modules.currencyprofilechanges.infrastructure.consumers.data
 
+import io.quarkus.runtime.annotations.RegisterForReflection
 import org.balhom.statisticsapi.common.data.enums.EventChangeTypeEnum
 import org.balhom.statisticsapi.modules.currencyprofilechanges.domain.models.CurrencyProfileSharedUser
 import org.balhom.statisticsapi.modules.currencyprofilechanges.domain.props.CurrencyProfileChangeProps
 import java.math.BigDecimal
 import java.util.UUID
 
+@RegisterForReflection
 data class CurrencyProfileChangeEvent(
     val action: String,
     val id: UUID,
